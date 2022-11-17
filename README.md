0df6e4b5d77add2cbbb569c207951572b9852b12
Chirag Sanghvi <chiragsanghvi10@gmail.com>
2022-11-18 00:09:04 +0100

    Formatting and Updating readme.md

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+Run Python file main.py and in browser open the following APIs
+
 1. GET : localhost:7003/api/v1/status
     
     - returns {"success": true}
@@ -6,3 +8,4 @@
       - page is the input integer to return 10 sentences in a list of dictionary.
 
       - If the page number is greater than (length of dataframe * 10)-1 the response is "THERE ARE ONLY : 100 TRANSLATIONS PLEASE ENTER PAGE NUMBER FROM 0 to 9 ONLY"
+
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from flask import Flask, jsonify, make_response, Response, json
 import pandas as pd
-from src.utils.loggers import get_logger
+from flask import Flask, jsonify, make_response, Response, json
+
 from src.services.translation_data import output_10_pairs
+from src.utils.loggers import get_logger
+
 logger = get_logger("main.py")
 
 app = Flask(__name__)
@@ -17,7 +19,7 @@ def get_status():
     """
     Returns the status of the server if it's still running.
     """
-    response = Response(json.dumps({'success':True}), mimetype='application/json')
+    response = Response(json.dumps({'success': True}), mimetype='application/json')
     response.status_code = 200
     logger.info("THE STATUS IS : ` {}`".format(response))
     return response
@@ -30,7 +32,7 @@ def translation_pairs(page):
     """
     try:
         response = output_10_pairs(df, page)
-        logger.info("10 TRANSLATIONS FROM : `{}` to `{}`".format((page*10), ((page*10)+10)))
+        logger.info("10 TRANSLATIONS FROM : `{}` to `{}`".format((page * 10), ((page * 10) + 10)))
         return make_response(jsonify(response), 200)
     except Exception as e:
         logger.info("ERROR in function.\n" + str(e))
